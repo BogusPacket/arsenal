695e882882aafce58d9862a3d41e7d97601e9273
parsemyarse <parsemyarse@gmail.com>
2017-06-29 06:00:40 +0000

    Made some changes to Libnet

--- includes/libpcap.h
+++ includes/libpcap.h
@@ -11,14 +11,22 @@ class Libpcap {
         bpf_u_int32 mask;
         pcap_t* handle;
         int lookupnet_flag;
+	struct bpf_program fp;
         Libpcap(){
 		dev = pcap_lookupdev(errbuf);
 		lookupnet_flag = pcap_lookupnet(dev, &net, &mask, errbuf);
-		handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
 		return;
 	};
 	~Libpcap(){
 	return;
+	}
+	void Sniff(pcap_handler callback, const char* filter, int count){
+		handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+		pcap_compile(handle, &fp, filter, 0, net);
+		pcap_setfilter(handle, &fp);
+		pcap_loop(handle, count, (pcap_handler) callback, NULL);
+		pcap_freecode(&fp);
+		pcap_close(handle);
 	}
 };
 
--- src/libpcap_test.cpp
+++ src/libpcap_test.cpp
@@ -2,13 +2,12 @@
 #include "../includes/libpcap.h"
 
 void got_packet(u_char* args, const struct pcap_pkthdr *header, const u_char *packet){
-	struct IPv4 ip;
-	memcpy(&ip.vhl, &packet + 14, sizeof(struct IPv4));
-	std::cout << IPv4_i2s(IPv4_n2h(ip.dst)) << std::endl;
+	struct IPv4* ip = (struct IPv4*)&packet + 14;
+	std::cout << IPv4_i2s(IPv4_n2h(ip->src)) << std::endl;
 }
 void libpcap_test(){
 	Libpcap l;
-	int num_packets = 10;
+	int num_packets = 100;
 	std::cout << COLOR_YELLOW << "Device: " << COLOR_RESET << l.dev  << std::endl;
-	pcap_loop(l.handle, num_packets, got_packet, NULL);
+	l.Sniff(got_packet, "ip", num_packets);
 }
