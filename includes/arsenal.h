d40bed7ea42c64bbc43b41b5b150237730926cd9
parsemyarse <parsemyarse@gmail.com>
2017-06-29 03:17:47 +0000

    Added src/ and includes/

---
+++ includes/arsenal.h
@@ -0,0 +1,15 @@
+#ifndef ARSENAL_H
+#define ARSENAL_H
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_BLUE "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN "\x1b[36m"
+#define COLOR_RESET "\x1b[0m"
+#include <bitset>
+#include <iostream>
+#include <sstream>
+unsigned int IPv4_n2h(unsigned int);
+std::string IPv4_i2s(unsigned int);
+#endif
---
+++ src/converter.cpp
@@ -0,0 +1,31 @@
+#include "../includes/arsenal.h"
+
+
+unsigned int IPv4_n2h(unsigned int arg){
+	register unsigned char r4 = *((unsigned char*) &arg);
+	register unsigned char r3 = *((unsigned char*) &arg + 1);
+	register unsigned char r2 = *((unsigned char*) &arg + 2);
+	register unsigned char r1 = *((unsigned char*) &arg + 3);
+	register unsigned int r;
+	*((unsigned char*) &r) = r1;
+	*((unsigned char*) &r + 1) = r2;
+	*((unsigned char*) &r + 2) = r3;
+	*((unsigned char*)&r + 3) = r4;
+	return r;;
+}
+
+std::string IPv4_i2s(unsigned int i){
+	register unsigned int r1 = *((unsigned char*) &i);
+        register unsigned int r2 = *((unsigned char*) &i + 1);
+        register unsigned int r3 = *((unsigned char*) &i + 2);
+        register unsigned int r4 = *((unsigned char*) &i + 3);
+	std::stringstream s;
+	s << r1;
+	s << ".";
+	s << r2;
+	s << ".";
+	s << r3;
+	s << ".";
+	s << r4;
+	return s.str();
+}
