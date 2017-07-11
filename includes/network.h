9bfdd59f2885959816693d86ea6f63c948f27065
parsemyarse <parsemyarse@gmail.com>
2017-07-11 13:22:52 -0700

    Update network.h

--- includes/network.h
+++ includes/network.h
@@ -5,4 +5,21 @@
 #define IPV4_DF 0x4000
 #define IPV4_MF 0x2000
 #define IPV4_OFFMASK 0x1ffff
+typedef struct Ethernet {
+	uint8_t dst[6];
+	uint8_t src[6];
+	uint16_t type;
+} Ethernet;
+
+typedef struct IPv4 {
+	uint8_t vhl;
+	uint8_t tos;
+	uint16_t len;
+	uint16_t offset;
+	uint8_t ttl;
+	uint8_t protocol;
+	uint16_t checksum;
+	uint32_t src;
+	uint32_t dst;
+} IPv4;
 #endif
