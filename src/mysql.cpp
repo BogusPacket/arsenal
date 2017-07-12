d078ddec2a9259d2775465f340ea384efaedf6d1
parsemyarse <parsemyarse@gmail.com>
2017-07-12 13:48:59 -0700

    Update mysql.cpp

--- src/mysql.cpp
+++ src/mysql.cpp
@@ -2,16 +2,16 @@
 
 class MySQL {
 private:
-  sql::Driver *driver;
-  sql::Connection *connection;
+  sql::Driver *driver = NULL;
+  sql::Connection *connection = NULL;
 public:
   MySQL(const char* host, int port, const char* user, const char* pass, char* dev){
     std::stringstream s;
     s << host;
-    s << ":"
+    s << ":";
     s << port
     cout << s;
     connection = driver->connect(s.c_str(), user, pass);
   }
   
-}
+};
