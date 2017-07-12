403cea1bda6e32900f93a0f8356f585a2f470343
parsemyarse <parsemyarse@gmail.com>
2017-07-12 14:27:45 -0700

    Update mysql.h

--- includes/mysql.h
+++ includes/mysql.h
@@ -1,14 +1,22 @@
 #ifndef MYSQL_H
 #define MYSQL_H
-#include "arsenal.h"
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/statement.h>
-class MySQL{
-  sql::Driver* driver;
-  sql::Connection* connection;
-  public:
-    MySQL(void, int, void, void);
+class MySQL {
+private:
+  sql::Driver *driver = NULL;
+  sql::Connection *connection = NULL;
+public:
+  template <typename S>
+  MySQL(S host, int port, S user, S pass, S dev){
+    std::stringstream s;
+    s << host;
+    s << ":";
+    s << port
+    cout << s;
+  }
+  
 };
 #endif
