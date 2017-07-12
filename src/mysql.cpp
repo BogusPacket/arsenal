8e727f0ee6c85a020eef0cbff0e871514c0629a0
parsemyarse <parsemyarse@gmail.com>
2017-07-12 14:12:19 -0700

    Update mysql.cpp

--- src/mysql.cpp
+++ src/mysql.cpp
@@ -5,8 +5,8 @@ private:
   sql::Driver *driver = NULL;
   sql::Connection *connection = NULL;
 public:
-  template <typename S, typename I>
-  MySQL(S host, I port, S user, S pass, S dev){
+  template <typename S>
+  MySQL(S host, int port, S user, S pass, S dev){
     std::stringstream s;
     s << host;
     s << ":";
