0afdf81cd50499f2d9ecf6022547c43a3187fe0d
parsemyarse <parsemyarse@gmail.com>
2017-07-13 12:10:13 -0700

    Update mysql.h

--- includes/mysql.h
+++ includes/mysql.h
@@ -4,15 +4,16 @@
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/statement.h>
+#define 
+template<typename H, typename U, typename P>
 class MySQL {
   private:
     sql::Driver *driver = NULL;
     sql::Connection *connection = NULL;
     
   public:
-    MySQL(char*, int, char*, char*);
-    MySQL(const char*, int, const char*, const char* const char*);
-    MySQL(char const*, int, char const*, char const*, char const*);
-    MySQL(std::string&, int, std::string&, std::string&, std::string&);
+    MySQL(H host, U user, P passwd, int){
+      
+    }
 };
 #endif
