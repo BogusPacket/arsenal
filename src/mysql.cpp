7616f39478a82a7949440fc50a1c59e80a4a2e4d
parsemyarse <parsemyarse@gmail.com>
2017-07-13 11:01:54 -0500

    Update mysql.cpp

--- src/mysql.cpp
+++ src/mysql.cpp
@@ -6,6 +6,8 @@ class MySQL {
     sql::Driver* driver;
     sql::Connection* connection
   public:
-    MySQL(char* host, int port, char* user, char* pass){}
-    MySQL(const char* host, int port, const char* user, const char* pass){}
+    MySQL(const char* host, int port, char* user, char* pass){
+    }
+    
 };
+#endif
