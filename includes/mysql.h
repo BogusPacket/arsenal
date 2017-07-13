23961bb1e4253ec7de29c1c2a54efc4ac852c651
parsemyarse <parsemyarse@gmail.com>
2017-07-13 12:39:04 -0700

    MySQL Connector

--- includes/mysql.h
+++ includes/mysql.h
@@ -4,16 +4,20 @@
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/statement.h>
+#define MYSQL_PROTO_TCP "tcp://"
+#define MYSQL_PROTO_UDP "udp://"
 #define 
 template<typename H, typename U, typename P>
 class MySQL {
   private:
     sql::Driver *driver = NULL;
     sql::Connection *connection = NULL;
-    
   public:
-    MySQL(H host, U user, P passwd, int){
-      
-    }
+	MySQL(H host, U user, P passwd, int port, const char proto){
+		string s = MYSQL_PROTO_TCP + host;
+		s += ":";
+		s += std::to_string(port);
+		std::cout << s;
+	}
 };
 #endif
--- test/mysql.cpp
+++ test/mysql.cpp
@@ -6,6 +6,6 @@ int main(int argc, char* argv[]){
   string host = "127.0.0.1";
   string user = "root";
   string pass = "choppersrus";
-  MySQL m<string>(host, 3306, user, pass);
+  MySQL m(host, user, pass, 3306, MYSQL_PROTO_TCP);
   return 1;
 }
