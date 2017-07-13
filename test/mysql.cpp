0750d6f1ce1ead5335f203c99955bd1dea4783c3
parsemyarse <parsemyarse@gmail.com>
2017-07-12 17:32:31 -0700

    Update mysql.cpp

--- test/mysql.cpp
+++ test/mysql.cpp
@@ -6,6 +6,6 @@ int main(int argc, char* argv[]){
   string host = "127.0.0.1";
   string user = "root";
   string pass = "choppersrus";
-  MySQL m(host, 3306, user, pass);
+  MySQL m<string>(host, 3306, user, pass);
   return 1;
 }
