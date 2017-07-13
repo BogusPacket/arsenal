86fc98f1f1c595e61c83557402f9370b05b525b4
parsemyarse <parsemyarse@gmail.com>
2017-07-13 12:47:07 -0700

    12:47

--- includes/mysql.h
+++ includes/mysql.h
@@ -6,15 +6,15 @@
 #include <cppconn/statement.h>
 #define MYSQL_PROTO_TCP "tcp://"
 #define MYSQL_PROTO_UDP "udp://"
-#define 
 template<typename H, typename U, typename P>
 class MySQL {
   private:
+    int port = 3306;
     sql::Driver *driver = NULL;
     sql::Connection *connection = NULL;
   public:
-	MySQL(H host, U user, P passwd, int port, const char proto){
-		string s = MYSQL_PROTO_TCP + host;
+	MySQL(H host, U user, P passwd){
+		std::string s = MYSQL_PROTO_TCP + host;
 		s += ":";
 		s += std::to_string(port);
 		std::cout << s;
