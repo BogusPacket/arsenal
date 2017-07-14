b72388f4e952dcdc595352773910b0537af8a05e
parsemyarse <parsemyarse@gmail.com>
2017-07-14 15:02:31 -0700

    Update mysql.h

--- includes/mysql.h
+++ includes/mysql.h
@@ -22,8 +22,9 @@ class MySQL<TCP> {
 		s += ":";
 		s += std::to_string(port);
 		std::cout << s;
-		driver = get_mysql_driver_instance();
+		driver = sql::mysql::get_mysql_driver_instance();
 		connection = driver->connect(host, user, passwd);
 	}
+	template<>
 };
 #endif
