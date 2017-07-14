c4d36925c0c7f10ded39e3932335cd1803ad5cfa
parsemyarse <parsemyarse@gmail.com>
2017-07-14 15:17:09 -0700

    Update mysql.cpp

--- test/mysql.cpp
+++ test/mysql.cpp
@@ -7,5 +7,8 @@ int main(int argc, char* argv[]){
   string user = "root";
   string pass = "choppersrus";
   MySQL<TCP> m(host, user, pass, 22);
+  m.switchDatabase("autopilot");
+  string s = "SELECT * FROM ITEMS";
+  m.execute(s);
   return 1;
 }
