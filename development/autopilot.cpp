d54ac5ae572ee8de27eb37bb3509f08b52dff067
parsemyarse <parsemyarse@gmail.com>
2017-07-17 14:42:55 -0700

    Update autopilot.cpp

--- development/autopilot.cpp
+++ development/autopilot.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <regex>
 #include <curl/curl.h>
 #define POPULAR_ITEMS "https://www.g2a.com/lucene/search/filter?jsoncallback=jQuery111007667557919303079_1500323788288&skip=&minPrice=0.00&maxPrice=900.00&cc=US&stock=all&event=bestseller&platform=0&search=&genre=0&cat=0&sortOrder=popularity+desc&start=0&rows=12&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=&includeFreeGames=false&isWholesale=false&_=1500323788290"
 
