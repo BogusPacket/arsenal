fd74b49e318b66ebd4e586d086af085a499b4a64
parsemyarse <parsemyarse@gmail.com>
2017-07-17 13:02:23 -0700

    Update autopilot.cpp

--- development/autopilot.cpp
+++ development/autopilot.cpp
@@ -1,20 +1,41 @@
-#include "arsenal.h"
-#include "mysql.h"
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
 
 class Autopilot{
 private:
-  sql::Connection* connection;
-  sql::Driver* driver;
   Curl* curl;
   CURLcode res;
 public:
   Autopilot(){
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
+    return;
   }
   void updateITEMS(){
-    #ifdef SKIP_PEER_VERIFICATION
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.g2a.com/");
+    int i = 0;
+    while (i < 10000) {
+      std::string url = "https://www.g2a.com/marketplace/product/auctions/?id=";
+      url += std::to_string(i);
+      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+     #ifdef SKIP_PEER_VERIFICATION
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+      #endif
+      #ifdef SKIP_HOSTNAME_VERIFICATION
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+      #endif
+      res = curl_easy_perform(curl);
+      if(res != CURLE_OK){
+        std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        curl_easy_cleanup(curl);
+      }
+      std::cout << res << std::endl;
+      i++;
+    }
+    curl_global_cleanup();
+    
   }
   ~Autopilot(){;}
 };
+
+int main()
