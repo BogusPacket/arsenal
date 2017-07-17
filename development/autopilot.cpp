0c80b7de9b073e4ba441559e3bd8ac0f146c671a
parsemyarse <parsemyarse@gmail.com>
2017-07-17 13:45:41 -0700

    Update autopilot.cpp

--- development/autopilot.cpp
+++ development/autopilot.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
-
+#define POPULAR_ITEMS "https://www.g2a.com/lucene/search/filter?jsoncallback=jQuery111007667557919303079_1500323788288&skip=&minPrice=0.00&maxPrice=900.00&cc=US&stock=all&event=bestseller&platform=0&search=&genre=0&cat=0&sortOrder=popularity+desc&start=0&rows=12&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=&includeFreeGames=false&isWholesale=false&_=1500323788290"
 class Autopilot{
 private:
   CURL* curl;
@@ -15,22 +15,20 @@ public:
   }
   void updateITEMS(){
     int i = 0;
-    while (i < 10000) {
-      char url[100] = "https://www.g2a.com/marketplace/product/auctions/?id=" + ;
-      strcat(&url[strlen(url) + 1], std::to_string(i);
-      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-     #ifdef SKIP_PEER_VERIFICATION
-      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-      #endif
-      #ifdef SKIP_HOSTNAME_VERIFICATION
-      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-      #endif
-      res = curl_easy_perform(curl);
-      if(res != CURLE_OK){
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-      }
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    char url[] = POPULAR_ITEMS;
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    #ifdef SKIP_PEER_VERIFICATION
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    #endif
+    #ifdef SKIP_HOSTNAME_VERIFICATION
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    #endif
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK){
+    printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    curl_easy_cleanup(curl);
+    }
+      printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
       i++;
     }
     curl_global_cleanup();
