e3bb3ff3a283e224b8c757a5b9a1fcddc7a335da
parsemyarse <parsemyarse@gmail.com>
2017-07-17 14:41:12 -0700

    Update autopilot.cpp

--- development/autopilot.cpp
+++ development/autopilot.cpp
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <string>
 #include <curl/curl.h>
 #define POPULAR_ITEMS "https://www.g2a.com/lucene/search/filter?jsoncallback=jQuery111007667557919303079_1500323788288&skip=&minPrice=0.00&maxPrice=900.00&cc=US&stock=all&event=bestseller&platform=0&search=&genre=0&cat=0&sortOrder=popularity+desc&start=0&rows=12&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=&includeFreeGames=false&isWholesale=false&_=1500323788290"
 
@@ -20,9 +19,8 @@ void updateITEMS(){
   #endif
   res = curl_easy_perform(curl);
   if(res != CURLE_OK){
-    printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
   }
-  printf("%s", res);
   curl_easy_cleanup(curl);
   curl_global_cleanup();
 }
