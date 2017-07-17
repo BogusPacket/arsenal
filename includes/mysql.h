f8babde3fde299acf15b9b928c6e03bffc8f8ed9
parsemyarse <parsemyarse@gmail.com>
2017-07-17 11:53:40 -0700

    Update mysql.h

--- includes/mysql.h
+++ includes/mysql.h
@@ -4,30 +4,3 @@
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/statement.h>
-#define TCP 1
-#define UDP 2
-template<const char P> class MySQL;
-
-template<> class MySQL<TCP> {
-  private:
-    int port = 3306;
-    sql::Driver *driver = NULL;
-    sql::Connection *con = NULL;
-  public:
-  	template <class H, class U, class P> MySQL(H host, U user, P passwd){
-		std::string s = "tcp://" ;
-		s += host;
-		s += ":";
-		s += std::to_string(port);
-		std::cout << s;
-		driver = get_driver_instance();
-		con = driver->connect(host, user, passwd);
-	}
-	int setPort(int p){
-		if (p < 65535 && p > 0){port = p; return 1;}
-		else {return 0;}
-	}
-	template<class D>void switchDatabase(D d){con->setSchema(d);}
-	template<class S> Query query(){return new Query;}
-};
-#endif
