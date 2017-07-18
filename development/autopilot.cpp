7145355c1af62c41e73712cc8a908832828794b4
parsemyarse <parsemyarse@gmail.com>
2017-07-18 15:13:37 -0700

    Update autopilot.cpp

--- development/autopilot.cpp
+++ development/autopilot.cpp
@@ -2,8 +2,17 @@
 #include <string>
 #include <regex>
 #include <curl/curl.h>
-#define POPULAR_ITEMS "https://www.g2a.com/lucene/search/filter?jsoncallback=jQuery111007667557919303079_1500323788288&skip=&minPrice=0.00&maxPrice=900.00&cc=US&stock=all&event=bestseller&platform=0&search=&genre=0&cat=0&sortOrder=popularity+desc&start=0&rows=12&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=&includeFreeGames=false&isWholesale=false&_=1500323788290"
-
+#define POPULAR_ITEMS "https://www.g2a.com/lucene/search/filter?jsoncallback=jQuery111007667557919303079_1500323788288&skip=&minPrice=0.00&maxPrice=900.00&cc=US&stock=all&event=bestseller&platform=0&search=&genre=0&cat=0&sortOrder=popularity+desc&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=&includeFreeGames=false&isWholesale=false&_=1500323788290&start="
+#define G2A_BESTSELLERS 0x01
+#define DYN "&start=0&rows=12"
+#define CURL_PREP(curl){    \
+  #ifdef SKIP_PEER_VERIFICATION     \
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);   \
+  #endif    \
+  #ifdef SKIP_HOSTNAME_VERIFICATION \
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);   \
+  #endif    \
+}   \
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -12,7 +21,8 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 }
 
 
-void updateITEMS_g2a_bestsellers(int num){
+void updateITEMS(int num){
+  
   int remainder = 0;
   if (!(num <= 24)){
     remainder = num % 24;
@@ -22,32 +32,58 @@ void updateITEMS_g2a_bestsellers(int num){
   curl_global_init(CURL_GLOBAL_DEFAULT);
   curl = curl_easy_init();
   std::string buf;
-  char url[] = POPULAR_ITEMS;
-  curl_easy_setopt(curl, CURLOPT_URL, url);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buf);
-  #ifdef SKIP_PEER_VERIFICATION
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-  #endif
-  #ifdef SKIP_HOSTNAME_VERIFICATION
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-  #endif
-  res = curl_easy_perform(curl);
-  if(res != CURLE_OK){
-    std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-  }
-  curl_easy_cleanup(curl);
+  if (remainder == 0) {
+    std::string url = POPULAR_ITEMS;
+    url += "&start=0&rows=";
+    url += std::to_string(num);
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buf);
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK){
+        std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+    }
+    curl_easy_cleanup(curl);
+    } else {
+        int i = num;
+        while (num){
+            std::string url;
+            if (i < 24){
+                url = POPULAR_ITEMS;
+                url += std::to_string(num - i);
+                url += "&rows="
+                url += std::to_string(i);
+                curl_easy_setopt(curl, CURLOPT_URL, url);
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buf);
+                i = 0;
+            } else {
+                url = POPULAR_ITEMS;
+                url += std::to_string(num - i);
+                url += "&rows=24";
+                i += 24;
+        }
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buf);
+        CURL_PREP(curl);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        }
+        curl_easy_cleanup(curl);
+    }
   curl_global_cleanup();
   std::regex r("(?:\"id\":)(\\d+)");
   std::smatch m;
   for(auto it = std::sregex_iterator(buf.begin(), buf.end(), r);
     it != std::sregex_iterator();
      ++it)
-{
+    {
     std::cout << "New Product ID:\t" << (*it)[1] << std::endl;
   }
 }
-int main(void){
+int main(55){
   updateITEMS();
   return 1;
 }
