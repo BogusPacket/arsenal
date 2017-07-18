3846561613da3cd42fd2256bd4e94c66a2fd6498
parsemyarse <parsemyarse@gmail.com>
2017-07-18 14:12:46 -0700

    Update autopilot.cpp

--- development/autopilot.cpp
+++ development/autopilot.cpp
@@ -4,6 +4,7 @@
 #include <curl/curl.h>
 #define POPULAR_ITEMS "https://www.g2a.com/lucene/search/filter?jsoncallback=jQuery111007667557919303079_1500323788288&skip=&minPrice=0.00&maxPrice=900.00&cc=US&stock=all&event=bestseller&platform=0&search=&genre=0&cat=0&sortOrder=popularity+desc&start=0&rows=12&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=&includeFreeGames=false&isWholesale=false&_=1500323788290"
 
+
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
@@ -11,7 +12,11 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 }
 
 
-void updateITEMS(){
+void updateITEMS_g2a_bestsellers(int num){
+  int remainder = 0;
+  if (!(num <= 24)){
+    remainder = num % 24;
+  }
   CURL* curl;
   CURLcode res;
   curl_global_init(CURL_GLOBAL_DEFAULT);
