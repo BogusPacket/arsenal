c51078ea1e7bf72dd5f31a092cfb4813d18d9342
parsemyarse <parsemyarse@gmail.com>
2017-07-18 11:43:06 -0500

    Update autopilot.cpp

--- development/autopilot.cpp
+++ development/autopilot.cpp
@@ -32,7 +32,7 @@ void updateITEMS(){
   }
   curl_easy_cleanup(curl);
   curl_global_cleanup();
-  std::regex r("\"id\":\\d+");
+  std::regex r("id(.*)");
   std::smatch m;
   std::regex_search (buf.c_str(),m,r);
   std::cout << m.str(1);
