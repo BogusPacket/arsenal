d40eb48fb772c99de21d2a94f554ae4e1beba08b
parsemyarse <parsemyarse@gmail.com>
2017-07-21 16:21:14 -0700

    Update arsenal.cpp

--- arsenal.cpp
+++ arsenal.cpp
@@ -10,6 +10,14 @@ int main(int argc, char* argv[]){
     return 1;
   }
   else if (argv[1] == "converter"){
-  
+    uint32_t addr = 0x64d2f82a;
+	  uint32_t switched_addr = IPv4_n2h(addr);
+	  std::cout << "addr == " << std::bitset<32>(addr) << std::endl;
+	  std::cout << "switched_addr == " << std::bitset<32>(switched_addr) << std::endl;
+	  std::string addr_s = IPv4_i2s(addr);
+	  std::string switched_addr_s = IPv4_i2s(switched_addr);
+	  std::cout << "IPv4_i2s(addr) " << addr_s << std::endl;
+	  std::cout << "IPv4_i2s(switched_addr) == " << switched_addr_s << std::endl;
+	  return 1;
   } 
 }
