38dbf454bfbab1dae9a4c4438452878d3faa2141
parsemyarse <parsemyarse@gmail.com>
2017-07-21 16:10:18 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -1,7 +1,4 @@
-#include "arsenal.h"
-#include<sys/socket.h>    //for socket ofcourse
-#include<cstdlib>
-#include<errno.h>
+#include<sys/socket.h>
 #include<netinet/tcp.h>   
 #include<netinet/ip.h>
 
