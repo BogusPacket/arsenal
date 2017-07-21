bbfea2c1df68d21f0244143073d03f5e51fbc80d
parsemyarse <parsemyarse@gmail.com>
2017-07-21 16:42:36 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -1,15 +1,19 @@
+#ifndef SOCKET_H
+#define SOCKET_H
 #include<sys/socket.h>
 #include<netinet/tcp.h>   
 #include<netinet/ip.h>
+#endif
 
-template<char const P> class Socket;
+/*template<char const P> class Socket;
 template<>
 class Socket<IPPROTO_UDP> {
   private:
     int fd;
-    char datagram[4096]
+    char datagram[4096];
   public:
     Socket(){
       fd = socket (PF_INET, SOCK_RAW, IPPROTO_UDP);
     }
 };
+*/
