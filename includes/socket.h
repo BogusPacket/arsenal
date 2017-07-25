1520d84d103254528db89266ce3202b85057c8d4
parsemyarse <parsemyarse@gmail.com>
2017-07-24 18:39:23 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -15,15 +15,12 @@ class Socket<TCP> {
     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     char datagram[4096];
     struct sockaddr_in server;
-    server.sin_family=AF_INET;
-    
 public:
-
     template <class D> Socket(D dst, int port){
       server.sin_addr.s_addr=inet_addr(dst);
       server.sin_port=htons(port);
-      connect(sock , (struct sockaddr *)&server , sizeof(server));}
-      
+      server.sin_family=AF_INET;
+      connect(sock , (struct sockaddr *)&server , sizeof(server));} 
     void setPort(int port){server.sin_port=htons(port);}
     template<class D> void setDst(D dst){server.sin_addr.s_addr=inet_addr(dst);}
     char* recieve(char* buf, size_t max){
