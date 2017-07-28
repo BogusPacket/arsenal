5af9e6dbcce42a1e9ad6dc1e97e23ebb0a4a4c86
parsemyarse <parsemyarse@gmail.com>
2017-07-27 18:08:22 -0700

    Update converter.h

--- includes/converter.h
+++ includes/converter.h
@@ -1,6 +1,11 @@
 #ifndef CONVERTER_H
 #define CONVERTER_H
 #include <bitset>
+#include <cstdlib>
+#include <cstring>
+
 unsigned int IPv4_n2h(unsigned int);
-std::string IPv4_i2s(unsigned int);
+template<typename I, typename S> S int2str(I i)
+template <unsigned int, typename S> S int2str(unsigned int i){S s; itoa(i, s, 0); return s;}
+template <>
 #endif
