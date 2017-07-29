282e584a351c75e877d969094685a890c962fa89
parsemyarse <parsemyarse@gmail.com>
2017-07-29 02:13:14 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -14,8 +14,8 @@ template<class P> class Socket : protected Arsenal {
       void dport(int port){server.sin_port=htons(port);}
       int CONNECT(){return connect(sock , (struct sockaddr *)&server , sizeof(server));};
       template <class Dst> void dst(Dst d){server.sin_addr.s_addr=inet_addr(dst);}
-      template <class Src> int listen(Src s){return bind(sock, (sockaddr*)&b, sizeof(b));;}
-      template <class Src> int listen(Src s, int port){b.sin_port=htons(port);}
+      template <class Src> int LISTEN(Src s){return bind(sock, (sockaddr*)&b, sizeof(b));;}
+      template <class Src> int LISTEN(Src s, int port){b.sin_port=htons(port);}
       Socket<P>(){b = {};}
 };
 
@@ -23,6 +23,7 @@ class UDP : public Socket<UDP> {
 public:
   UDP(){server.sin_family=AF_INET;
         this->sock = socket(AF_INET, SOCK_DGRAM, 0);}
+  template<class Msg> SEND(Msg m, int length){return sendto(this->sock, (const char*) m, length, 0, (sockaddr*)&this->server, sizeof(this->server));}
 };
 
 class TCP : public Socket<TCP> {
