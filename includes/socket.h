3c4dc8a6d4e3ee4018bc59d7dce58e14e57c6494
parsemyarse <parsemyarse@gmail.com>
2017-07-29 01:08:42 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -13,10 +13,6 @@ template<class P> class Socket : protected Arsenal {
   public:
       void setPort(int port){server.sin_port=htons(port);}
       Socket(){;}
-      template<class Dst> setDst(Dst dst){server.sin_addr.s_addr=inet_addr(dst);}
-      template<class Buf> recieve(Buf* buf, size_t max){
-        int bytesRecieved = 0, totalBytesRecieved = 0;
-        while (!((bytesRecieved=recv(sock, buf, 65535, MSG_DONTWAIT))<=0)){totalBytesRecieved += bytesRecieved;}}   */
 };
 
 template<> class TCP : public Socket<TCP> {
