7337321d4822663a0eae33040bdfbd015658d795
parsemyarse <parsemyarse@gmail.com>
2017-07-29 01:42:24 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -1,21 +1,21 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 #include "arsenal.h"
-#define TCP Tcp
+class TCP;
 
 template<class P> class Socket : protected Arsenal {
   protected:
-    P p;
     int sock;
     char datagram[4096];
     struct sockaddr_in server;
   public:
       void setPort(int port){server.sin_port=htons(port);}
-      Socket(){p = this;}
+      Socket<P>(){P p;}
 };
 
-class Tcp : public Socket<TCP> {
+class TCP : public Socket<TCP> {
+  public:
   TCP(){this->sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        this->server.sin_family=AF_INET;};
-};
+        this->server.sin_family=AF_INET;}};
+
 #endif
