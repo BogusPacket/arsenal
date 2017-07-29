c1e49e2b55d0e72b9953659f6e93e3d77595dd59
parsemyarse <parsemyarse@gmail.com>
2017-07-29 01:06:25 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -1,35 +1,26 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 #include "arsenal.h"
-#define TCP 0x01
-#define UDP 0x02
+
+class TCP;
+class UDP;
 
 template<class P> class Socket : protected Arsenal {
   private:
     int sock;
     char datagram[4096];
     struct sockaddr_in server;
-  
-    //PRIVATE FUNCTION TEMPLATE DECLARATIONS
-    int TCP_INIT(char* dst, int port);
   public:
       void setPort(int port){server.sin_port=htons(port);}
-      Socket()
-      /*template<class Dst> Socket(Dst dst, int port);
+      Socket(){;}
       template<class Dst> setDst(Dst dst){server.sin_addr.s_addr=inet_addr(dst);}
       template<class Buf> recieve(Buf* buf, size_t max){
         int bytesRecieved = 0, totalBytesRecieved = 0;
         while (!((bytesRecieved=recv(sock, buf, 65535, MSG_DONTWAIT))<=0)){totalBytesRecieved += bytesRecieved;}}   */
 };
 
-int Socket::TCP_INIT(char* dst){
-  sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  server.sin_addr.s_addr=inet_addr(dst);
-  //server.sin_port=htons(port);
-  server.sin_family=AF_INET;
-  connect(sock , (struct sockaddr *)&server , sizeof(server));}
-
-template<char const> Socket<TCP>::Socket(char const* dst, int port){
-  server.sin_port=htons(port);
-}
+template<> class TCP : public Socket<TCP> {
+  TCP(){this->sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        this->server.sin_family=AF_INET;};
+};
 #endif
