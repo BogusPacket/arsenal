c027abb09d9f8c0e8c7dc190368a882b7b69f224
parsemyarse <parsemyarse@gmail.com>
2017-07-28 22:53:36 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -4,27 +4,32 @@
 #define TCP 0x01
 #define UDP 0x02
 
-
-template<char const P> class Socket : protected Arsenal {
+template<class P> class Socket : protected Arsenal {
   private:
-  int sock;
-  char datagram[4096];
-  struct sockaddr_in server;
+    int sock;
+    char datagram[4096];
+    struct sockaddr_in server;
+  
+    //PRIVATE FUNCTION TEMPLATE DECLARATIONS
+    int TCP_INIT(char* dst, int port);
   public:
-    void setPort(int port){server.sin_port=htons(port);}
-    template<class Dst> Socket(Dst dst, int port);
-    template<class Dst> setDst(Dst dst){server.sin_addr.s_addr=inet_addr(dst);}
-    template<class Buf> recieve(Buf* buf, size_t max){
-      int bytesRecieved = 0, totalBytesRecieved = 0;
-      while (!((bytesRecieved=recv(sock, buf, 65535, MSG_DONTWAIT))<=0)){totalBytesRecieved += bytesRecieved;}}   
+      void setPort(int port){server.sin_port=htons(port);}
+      Socket()
+      /*template<class Dst> Socket(Dst dst, int port);
+      template<class Dst> setDst(Dst dst){server.sin_addr.s_addr=inet_addr(dst);}
+      template<class Buf> recieve(Buf* buf, size_t max){
+        int bytesRecieved = 0, totalBytesRecieved = 0;
+        while (!((bytesRecieved=recv(sock, buf, 65535, MSG_DONTWAIT))<=0)){totalBytesRecieved += bytesRecieved;}}   */
 };
 
-template<> template<class Dst> class Socket<TCP>::Socket((Dst) dst, int port){
-      sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-      server.sin_addr.s_addr=inet_addr(dst);
-      server.sin_port=htons(port);
-      server.sin_family=AF_INET;
-      connect(sock , (struct sockaddr *)&server , sizeof(server));}
+int Socket::TCP_INIT(char* dst){
+  sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  server.sin_addr.s_addr=inet_addr(dst);
+  //server.sin_port=htons(port);
+  server.sin_family=AF_INET;
+  connect(sock , (struct sockaddr *)&server , sizeof(server));}
 
-
+template<char const> Socket<TCP>::Socket(char const* dst, int port){
+  server.sin_port=htons(port);
+}
 #endif
