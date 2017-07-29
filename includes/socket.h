cb587f5278417b489b258d867cb16ff5b0754910
parsemyarse <parsemyarse@gmail.com>
2017-07-28 22:15:44 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -5,25 +5,32 @@
 #define UDP 0x02
 
 
+template<char const P> class Socket : protected Arsenal {
+  private:
+  int sock;
+  char datagram[4096];
+  struct sockaddr_in server;
+  public:
+    template<class Dst> Socket(Dst dst, int port);
+    template<class Buf> recieve(Buf* buf, size_t max);
+};
 
-class Socket : protected Arsenal {
+
+template<> class Socket<TCP> : protected Arsenal {
   private:
     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    char datagram[4096];
-    struct sockaddr_in server;
-public:
-      template <char const Type, class Dst> Socket<Type t>(Dst dst, int port);
-      void setPort(int port){server.sin_port=htons(port);}
-      template<class Dst> void setDst(Dst dst){server.sin_addr.s_addr=inet_addr(dst);}
-      template<class Buf> int recieve(Buf* buf, size_t max){
-      int bytesRecieved = 0, totalBytesRecieved = 0;
-      while (!((bytesRecieved=recv(sock, buf, 65535, MSG_DONTWAIT))<=0)){totalBytesRecieved += bytesRecieved;}}
-};
-  template<class Dst> Socket::Socket<TCP>(Dst dst, int port){
+  public:
+      template<class Dst> Socket<TCP>(Dst dst, int port){
       server.sin_addr.s_addr=inet_addr(dst);
       server.sin_port=htons(port);
       server.sin_family=AF_INET;
-      connect(sock , (struct sockaddr *)&server , sizeof(server));
-  }
+      connect(sock , (struct sockaddr *)&server , sizeof(server));}
+};
+
+template<typename P> void Socket<P>::setPort(int port){server.sin_port=htons(port);}
+template<typename P> template<class Dst> void Socket<P>::setDst(Dst dst){server.sin_addr.s_addr=inet_addr(dst);}
+template<typename P> template<class Buf> int Socket<P>::recieve(Buf* buf, size_t max){
+      int bytesRecieved = 0, totalBytesRecieved = 0;
+      while (!((bytesRecieved=recv(sock, buf, 65535, MSG_DONTWAIT))<=0)){totalBytesRecieved += bytesRecieved;}}
 
 #endif
