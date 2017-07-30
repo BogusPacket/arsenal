b0734c6c57e11385289b8d3f0edf34d6f36b58fa
parsemyarse <parsemyarse@gmail.com>
2017-07-30 12:35:12 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -4,7 +4,28 @@
 class TCP;
 class UDP;
 
-struct DNS_QUERY {};
+struct DNS_HEADER
+{
+    unsigned short id; // identification number
+ 
+    unsigned char rd :1; // recursion desired
+    unsigned char tc :1; // truncated message
+    unsigned char aa :1; // authoritive answer
+    unsigned char opcode :4; // purpose of message
+    unsigned char qr :1; // query/response flag
+ 
+    unsigned char rcode :4; // response code
+    unsigned char cd :1; // checking disabled
+    unsigned char ad :1; // authenticated data
+    unsigned char z :1; // its z! reserved
+    unsigned char ra :1; // recursion available
+ 
+    unsigned short q_count; // number of question entries
+    unsigned short ans_count; // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count; // number of resource entries
+};
+ 
 
 template<class P> class Socket : protected Arsenal {
   protected:
@@ -28,7 +49,9 @@ public:
   UDP(){server.sin_family=AF_INET;
         this->sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);}
         template <class Msg> int SEND(Msg m, int length){return sendto(this->sock, m, length, MSG_DONTWAIT, (sockaddr*)&this->server, sizeof(this->server));}
-        void RECV(void* buf, size_t size){socklen_t fromlen = sizeof(sockaddr); ssize_t i = recvfrom(this->sock, buf, size, MSG_DONTWAIT, (sockaddr*)&this->b, &fromlen);}
+        void RECV(void* buf, size_t size){socklen_t fromlen = sizeof(sockaddr);
+                                          ssize_t i = recvfrom(this->sock, buf, size, MSG_DONTWAIT, (sockaddr*)&this->b, &fromlen);
+                                          struct DNS_HEADER* dns; dns = (struct DNS_HEADER*) &buf;}
 };
 
 class TCP : public Socket<TCP> {
