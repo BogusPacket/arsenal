8e8fd743dc4bd4fa967bc5366e75b92faca5ad54
parsemyarse <parsemyarse@gmail.com>
2017-07-30 11:12:49 -0700

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -25,7 +25,7 @@ class UDP : public Socket<UDP> {
 public:
   UDP(){server.sin_family=AF_INET;
         this->sock = socket(AF_INET, SOCK_DGRAM, 0);}
-        template <class Msg> int SEND(Msg m, int length){return sendto(this->sock, m, length, 0, (sockaddr*)&this->server, sizeof(this->server));}
+        template <class Msg> int SEND(Msg m, int length){return sendto(this->sock, m, length, MSG_DONTWAIT, (sockaddr*)&this->server, sizeof(this->server));}
         void RECV(void* buf, size_t size){socklen_t fromlen = sizeof(sockaddr); ssize_t i = recvfrom(this->sock, buf, size, 0, (sockaddr*)&this->server, &fromlen);}
 };
 
