b71e70f035114dfc6ab26cfab0d17125fb3d5180
parsemyarse <parsemyarse@gmail.com>
2017-08-25 21:28:03 -0500

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -14,23 +14,28 @@ const unsigned char UDP_ECHO[] = {0x0d, 0x0a, 0x0d, 0x0a};
 struct DNS_HEADER
 {
     unsigned short id; // identification number
-    unsigned char rd :1; // recursion desired
-    unsigned char tc :1; // truncated message
-    unsigned char aa :1; // authoritive answer
-    unsigned char opcode :4; // purpose of message
-    unsigned char qr :1; // query/response flag
-    unsigned char rcode :4; // response code
-    unsigned char cd :1; // checking disabled
-    unsigned char ad :1; // authenticated data
-    unsigned char z :1; // its z! reserved
-    unsigned char ra :1; // recursion available
-    unsigned short q_count; // number of question entries
-    unsigned short ans_count; // number of answer entries
-    unsigned short auth_count; // number of authority entries
-    unsigned short add_count; // number of resource entries
+    unsigned char rd :0; // recursion desired
+    unsigned char tc :0; // truncated message
+    unsigned char aa :0; // authoritive answer
+    unsigned char opcode :0; // purpose of message
+    unsigned char qr :0; // query/response flag
+    unsigned char rcode :0; // response code
+    unsigned char cd :0; // checking disabled
+    unsigned char ad :0; // authenticated data
+    unsigned char z :0; // its z! reserved
+    unsigned char ra :0; // recursion available
+    unsigned short q_count :0; // number of question entries
+    unsigned short ans_count :0; // number of answer entries
+    unsigned short auth_count :0; // number of authority entries
+    unsigned short add_count :0; // number of resource entries
 };
  
-
+struct QUESTION
+{
+    unsigned short qtype;
+    unsigned short qclass;
+};
+ 
 template<class P> class Socket : protected Arsenal {
   protected:
     int sock;
