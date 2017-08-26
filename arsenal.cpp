55a1494fbff804b20b04f7cb6fe45fe5750930d8
parsemyarse <parsemyarse@gmail.com>
2017-08-25 21:57:17 -0500

    Update arsenal.cpp

--- arsenal.cpp
+++ arsenal.cpp
@@ -43,8 +43,8 @@ else if (strcmp(argv[1], "dns") == 0){
 	qu->qtype = htons(1);
 	sock.SEND(buf, sizeof(buf));
 	char buf1[100];
-	sock.recv(buf1, 100);
-	std::cout << buf1 << std::endl
+	sock.RECV(buf1, 100);
+	std::cout << buf1 << std::endl;
 }
 return 1;}
 /*else if (strcmp(argv[1], "converter") == 0){
