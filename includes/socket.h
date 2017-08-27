f13015cc605cabcf1427c467686b902a23451400
parsemyarse <parsemyarse@gmail.com>
2017-08-26 20:34:27 -0500

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -14,19 +14,16 @@ const unsigned char UDP_ECHO[] = {0x0d, 0x0a, 0x0d, 0x0a};
 struct DNS_HEADER
 {
     unsigned short id; // identification number
- 
     unsigned char rd :1; // recursion desired
     unsigned char tc :1; // truncated message
     unsigned char aa :1; // authoritive answer
     unsigned char opcode :4; // purpose of message
     unsigned char qr :1; // query/response flag
- 
     unsigned char rcode :4; // response code
     unsigned char cd :1; // checking disabled
     unsigned char ad :1; // authenticated data
     unsigned char z :1; // its z! reserved
     unsigned char ra :1; // recursion available
- 
     unsigned short q_count; // number of question entries
     unsigned short ans_count; // number of answer entries
     unsigned short auth_count; // number of authority entries
@@ -39,7 +36,7 @@ struct DNS_QUESTION
     unsigned short qtype;
     unsigned short qclass;
 };
- 
+void printDNS_HEADER(struct DNS_HEADER* h);
 template<class P> class Socket : protected Arsenal {
   protected:
     int sock;
