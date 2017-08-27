60196f36e0928c6fe73782ec14d14ba5ada3b8db
parsemyarse <parsemyarse@gmail.com>
2017-08-26 21:07:15 -0500

    Update arsenal.cpp

--- arsenal.cpp
+++ arsenal.cpp
@@ -18,7 +18,7 @@ else if (strcmp(argv[1], "dns") == 0){
         //sock.SEND(UDP_DNSSTATUS, sizeof(UDP_DNSSTATUS));
         //struct DNS_HEADER* dns = (struct DNS_HEADER*) &buf;
 
-        char name[] = {"3www11binarytides3com"};
+        unsigned char name[] = {"3www6google3com"};
         char buf[sizeof(DNS_HEADER) + sizeof(DNS_QUESTION) + sizeof(name)];
         DNS_HEADER *dns = (struct DNS_HEADER*) &buf[0];
         dns->id = (unsigned short) htons(1);
