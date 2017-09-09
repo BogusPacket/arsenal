0bd5b07ee3d06a6e5d54ec5108ead052b039d5de
parsemyarse <parsemyarse@gmail.com>
2017-09-09 17:50:23 -0500

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -65,7 +65,38 @@ unsigned short qclass;};
 unsigned int randIPV4(unsigned int ip);
 void printDNS_HEADER(struct DNS_HEADER* h);
 void ChangetoDnsNameFormat(unsigned char* dns, unsigned char* host);
-template<class Name, class Buf> void DNSLookup(Name n, Buf b);
+template<class Name, class Buf> void DNSLookup(Name name, Buf b){
+  UDP sock;
+  sock.dst("8.8.8.8");
+  sock.dport(53);
+  sock.src("104.36.18.123");
+  sock.LISTEN("104.36.18.123", 53);
+  unsigned char buf[65535];
+  DNS_HEADER *dns = (struct DNS_HEADER*) &buf[0];
+  dns->id = (unsigned short) htons(768);
+  dns->qr = 0; //This is a query
+  dns->opcode = 0; //This is a standard query
+  dns->aa = 0; //Not Authoritative
+  dns->tc = 0; //This message is not truncated
+  dns->rd = 1; //Recursion Desired
+  dns->ra = 0; //Recursion not available! hey we dont have it (lol)
+  dns->z = 0;
+  dns->ad = 0;
+  dns->cd = 0;
+  dns->rcode = 0;
+  dns->q_count = htons(1); //we have only 1 question
+  dns->ans_count = 0;
+  dns->auth_count = 0;
+  dns->add_count = 0;
+  unsigned char* qname = (unsigned char*)&buf[sizeof(struct DNS_HEADER)];
+  ChangetoDnsNameFormat(qname, name);
+  struct DNS_QUESTION* qu = (struct DNS_QUESTION*) &buf[sizeof(DNS_HEADER) + (strlen((const char*)qname) + 1)];
+  qu->qclass = htons(1);
+  qu->qtype = htons(1);
+  sock.SEND(buf, sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct DNS_QUESTION));
+  unsigned char buf1[100];
+  sock.RECV(buf1, 100);
+  printDNS_HEADER((struct DNS_HEADER*) &buf1[0]);}
 template<class P> class Socket : protected Arsenal {
   protected:
     int sock;
