8debb72418edc904d9c57e51d7d9da1b6a7c9134
Nash Sheldon <parsemyarse@gmail.com>
2017-10-16 20:54:20 -0500

    Update CSGOItems.py

--- python/steamedit/CSGOItems.py
+++ python/steamedit/CSGOItems.py
@@ -1,16 +1,64 @@
-import Item
+from steamkit.SteamClient import SteamClient
 class CSGOItem(SteamClient):
-	def __init__(self, name='', steamid='', itemid='', assetid=''):
+	def __init__(self, name=None, steamid=None, itemid=None, assetid=None, price=None, fee=None, float=None):
 		self.name=name
     		self.marketid=marketid
     		self.assetid=assetid
 		self.inspect=inspect
-    		self.steamid=steamid		
+    		self.steamid=steamid
+		self.float=float
+		self.fee=fee
+		self.price=price
+		
 	def getFloat(self, *extrasteamclients): #getFloat will alternate between steam clients
-		cs.send(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockRequest, {
+		self.csgo.send(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockRequest, {
                     'param_s': self.steamid,
                     'param_a': self.assetid,
                     'param_d': self.itemid),
                     'param_m': self.marketid,})
 		response, = cs.wait_event(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockResponse)
-		return struct.unpack("f", struct.pack("i", response.iteminfo.paintwear))[0]
+		self.float=struct.unpack("f", struct.pack("i", response.iteminfo.paintwear))[0]
+		
+def getLowestFloatSkins(skin, start, cnt):
+	global url
+	url = "http://steamcommunity.com/market/listings/730/" + skin + "/render/?query=&start=" + str(start) + "&count=" + str(cnt) + "&country=US&language=english&currency=1"
+	url = url.replace(" ", "%20")
+	url = url.replace("|", "%7C")
+	url = url.replace("(", "%28")
+	url = url.replace(")", "%29")
+	print url
+	txt = urllib.urlopen(url).read()
+	r = re.split("{\"link\":\"", txt)
+	i = 0
+	links = []
+	while 1:
+		try:
+			s = r[i].split("\",\"")[0]
+			if "listing" not in s:
+				if s not in links:
+					if "csgo_econ" in s:
+						links.append(s)
+			i += 1
+		except: break
+	
+	marketids=[]
+	itemids=[]
+	assetids=[]
+	prices[]
+	fees=[]
+	for each in links:
+		marketid = each.split("M")[1].split("A")[0]
+		itemids.append(each.split("D")[1])
+		assetids.append(txt.split(marketid + "_name\', 730, \'2\', \'")[1].split("\',")[0])
+		marketids.append(marketid)
+	prices = re.findall("market_listing_price market_listing_price_with_fee.{20}([0-9]+\.[0-9]+)", txt)
+	i = 0
+	for each in re.findall("market_listing_price market_listing_price_without_fee.{20}([0-9]+\.[0-9]+)", txt):
+		fees.append(str(int(prices[i]) - int(each)))
+		i += 1
+	items = []
+	i = 0
+	for each in links:
+		items.append(CSGOItem(marketid=marketids[i], itemid=itemids[i], price=prices[i], fee=fees[i]))
+		i += 1
+	return items
