53dd92b40e7f9f7cac8a753de3f9649ce7d594c0
Nash Sheldon <parsemyarse@gmail.com>
2017-10-16 21:16:54 -0500

    Update and rename DesktopClient.py to clients.py

--- python/steamedit/clients.py
+++ python/steamedit/clients.py
@@ -1,9 +1,12 @@
-class DesktopClient:
+#!/usr/bin/python
+import steam
+import csgo
+class Desktop:
 	def __init__(self, username='', password=''):
 		self.username = username
 		self.password = password
 		self.client = steam.SteamClient()
-		self.csgo = CSGOClient(client)
+		self.csgo = csgo.CSGOClient(client)
 		self.client.cli_login(username=username, password=password)
 		self.client.run_forever()
 	def startCSGO(self):
