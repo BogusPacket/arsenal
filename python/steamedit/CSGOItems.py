7bdb1e29d4da1b53d990816fa0f473d9d48d1da8
Nash Sheldon <parsemyarse@gmail.com>
2017-10-16 19:59:40 -0500

    Update and rename csgoitems.py to CSGOItems.py

--- python/steamedit/CSGOItems.py
+++ python/steamedit/CSGOItems.py
@@ -1,15 +1,16 @@
-class Item(SteamClient):
-	def __init__(self, marketid='', steamid='', itemid='', assetid=''):
-    self.marketid=marketid
-    self.assetid=assetid
+import Item
+class CSGOItem(SteamClient):
+	def __init__(self, name='', steamid='', itemid='', assetid=''):
+		self.name=name
+    		self.marketid=marketid
+    		self.assetid=assetid
 		self.inspect=inspect
-    self.steamid=steamid
+    		self.steamid=steamid		
 	def getFloat(self, *extrasteamclients): #getFloat will alternate between steam clients
 		cs.send(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockRequest, {
                     'param_s': self.steamid,
                     'param_a': self.assetid,
                     'param_d': self.itemid),
-                    'param_m': self.marketid,
-                 })
+                    'param_m': self.marketid,})
 		response, = cs.wait_event(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockResponse)
 		return struct.unpack("f", struct.pack("i", response.iteminfo.paintwear))[0]
