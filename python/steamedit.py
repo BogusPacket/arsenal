c2ca518d182524fc2e56655d79fabafff1664d82
Nash Sheldon <parsemyarse@gmail.com>
2017-10-16 21:42:48 -0500

    Update steamedit.py

--- python/steamedit.py
+++ python/steamedit.py
@@ -70,6 +70,6 @@ def getSkinListings(skin, start, cnt):
 	items = []
 	i = 0
 	for each in links:
-		items.append(Item(marketid=marketids[i], itemid=itemids[i], price=prices[i], fee=fees[i]))
+		items.append(Skin(marketid=marketids[i], itemid=itemids[i], price=prices[i], fee=fees[i]))
 		i += 1
 	return items
