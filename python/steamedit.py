2919f5ad1373d4800b388822a6c47c9f6e435442
Nash Sheldon <parsemyarse@gmail.com>
2017-10-16 21:28:57 -0500

    Create steamedit.py

---
+++ python/steamedit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import steam
+import csgo
+class DesktopClient:
+	def __init__(self, username='', password=''):
+		self.username = username
+		self.password = password
+		self.client = steam.SteamClient()
+		self.csgo = csgo.CSGOClient(self.client)
+		self.client.cli_login(username=username, password=password)
+	def startCSGO(self):
+		self.csgo.launch()
