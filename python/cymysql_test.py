0f6c730fd879e9985c0a6ee03bf43603572198b1
Nash Sheldon <parsemyarse@gmail.com>
2017-10-19 10:06:45 -0500

    Delete cymysql_test.py

--- python/cymysql_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import cymysql
-
-c = cymysql.connect(host='localhost', port=3306, user='root', passwd='choppersrus', db='autopilot')
-cursor = c.cursor()
-cursor.execute("SELECT name FROM ITEMS WHERE g2a_product_id = 94")
-for each in cursor:
-	print(str(each))
