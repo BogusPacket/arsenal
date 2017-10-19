1799672d13c345607e842fa1c4b3822f49b7bf9d
Nash Sheldon <parsemyarse@gmail.com>
2017-10-18 19:41:44 -0500

    Update steamedit.py

--- python/steamedit.py
+++ python/steamedit.py
@@ -48,7 +48,6 @@ class Skin:
                     'param_m': int(self.marketid),})
 		response, = client.csgo.wait_event(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockResponse)
 		self.float=struct.unpack("f", struct.pack("i", response.iteminfo.paintwear))[0]
-		return self.float
 		
 def getSkinListings(skin, start, cnt):
 	global url
